e89905e9746c728ce438d93dc31fe24d63a128c5
Carmine <>
2021-11-23 20:07:52 +0100

    ComplexNumOperation & StackManipulation

---
+++ ScientificCalculator/src/scientificcalculator/StackManipulation.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scientificcalculator;
+
+import java.util.Deque;
+import org.apache.commons.math3.complex.Complex;
+import java.util.NoSuchElementException;
+
+/**
+ *
+ * @author Group 10
+ */
+public class StackManipulation {
+    
+    private Deque<Complex> stack;
+
+    public StackManipulation(Deque<Complex> stack) {
+        this.stack = stack;
+    }
+    
+    public void clear() throws NoSuchElementException{
+        
+        stack.clear();
+
+    }
+    
+    public void drop() throws NoSuchElementException{
+        
+        stack.removeFirst();
+
+    }
+
+    public void dup() throws NoSuchElementException{
+        
+        Complex op1 = stack.removeFirst();
+        
+        stack.addFirst(op1);
+        stack.addFirst(op1);
+        
+    }
+    
+    public void swap() throws NoSuchElementException{
+        
+        Complex op1 = stack.removeFirst();
+        Complex op2 = stack.removeFirst();
+        
+        stack.addFirst(op1);
+        stack.addFirst(op2);
+        
+    }
+    
+    public void over() throws NoSuchElementException{
+        
+        Complex op1 = stack.removeFirst();
+        Complex op2 = stack.removeFirst();
+        
+        stack.addFirst(op2);
+        stack.addFirst(op1);
+        stack.addFirst(op2);
+        
+    }
+}

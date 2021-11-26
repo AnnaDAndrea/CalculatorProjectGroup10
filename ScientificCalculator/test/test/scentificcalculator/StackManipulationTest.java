03814b24f4d17aedd7653c6d0e3598500b67bfcb
Carmine Coppola <>
2021-11-26 12:16:28 +0100

    StackManipulationTest v2

--- ScientificCalculator/test/test/scentificcalculator/StackManipulationTest.java
+++ ScientificCalculator/test/test/scentificcalculator/StackManipulationTest.java
@@ -5,12 +5,14 @@
  */
 package test.scentificcalculator;
 
-import org.junit.After;
-import org.junit.AfterClass;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import org.apache.commons.math3.complex.Complex;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import scientificcalculator.StackManipulation;
 
 /**
  *
@@ -18,28 +20,103 @@ import static org.junit.Assert.*;
  */
 public class StackManipulationTest {
     
+    private StackManipulation s;
+    private Deque<Complex> stack;
+    
     public StackManipulationTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
     }
     
     @Before
     public void setUp() {
+        stack = new LinkedList<>();
+        s = new StackManipulation(stack);
     }
     
-    @After
-    public void tearDown() {
+    @Test
+    public void testClear() {
+        s.clear();
+        assertEquals(true, stack.isEmpty());
+    }
+    
+    @Test
+    public void testDrop() {
+        stack.addFirst(new Complex(3,3));
+        int s1 = stack.size();
+        s.drop();
+        assertEquals(s1 - 1, stack.size());
+    }
+    
+    @Test(expected = NoSuchElementException.class)
+    public void testDropFail() {
+        int s1 = stack.size();
+        s.drop();
+        assertEquals(s1, stack.size());      
+    }
+    
+    @Test
+    public void testDup() {
+        Complex c = new Complex(3,3);
+        stack.addFirst(c);
+        int s1 = stack.size();
+        s.dup();
+        assertEquals(s1 + 1, stack.size());
+        assertEquals(stack.peekFirst(), c);
+    }
+    
+    @Test
+    public void testDup2() {
+        Complex c = new Complex(3,3);
+        Complex c1 = new Complex(5,3);
+        stack.addFirst(c);
+        stack.addFirst(c1);
+        int s1 = stack.size();
+        s.dup();
+        assertEquals(s1 + 1, stack.size());
+        assertEquals(stack.peekFirst(), c1);
+    }
+        
+    @Test(expected = NoSuchElementException.class)
+    public void testDupFail() {
+        int s1 = stack.size();
+        s.dup();
+        assertEquals(s1, stack.size());     
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void testSwap() {
+        Complex c1 = new Complex(3,3);
+        Complex c2 = new Complex(5,5);
+        stack.addFirst(c1);
+        stack.addFirst(c2);
+        int s1 = stack.size();
+        s.swap();
+        assertEquals(s1, stack.size());
+        assertEquals(stack.peekFirst(), c1);
+    } 
+        
+    @Test(expected = NoSuchElementException.class)
+    public void testSwapFail() {
+        s.swap();
+        assertEquals(0, stack.size());     
+    }
+    
+    @Test
+    public void testOver() {
+        Complex c1 = new Complex(3,3);
+        Complex c2 = new Complex(5,5);
+        stack.addFirst(c1);
+        stack.addFirst(c2);
+        int s1 = stack.size();
+        s.over();
+        assertEquals(s1 + 1, stack.size());
+        assertEquals(stack.peekFirst(), c1);
+    } 
+    
+    @Test(expected = NoSuchElementException.class)
+    public void testOverFail() {
+        int s1 = stack.size();
+        s.over();
+        assertEquals(s1, stack.size());  
+    } 
 }
+    

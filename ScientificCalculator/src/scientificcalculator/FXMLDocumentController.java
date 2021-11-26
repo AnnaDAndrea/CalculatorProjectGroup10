0cb67c680439a133ed3a286c2fbc966cf7658f0c
Anna D'Andrea <>
2021-11-26 11:14:30 +0100

    Controller updated

--- ScientificCalculator/src/scientificcalculator/FXMLDocumentController.java
+++ ScientificCalculator/src/scientificcalculator/FXMLDocumentController.java
@@ -15,12 +15,14 @@ import java.util.logging.Logger;
 import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import org.apache.commons.math3.complex.*;
 
@@ -40,6 +42,34 @@ public class FXMLDocumentController implements Initializable {
     private Button pointButton;
     @FXML
     private Button sendButton;
+    @FXML
+    private TextField displayField;
+    @FXML
+    private Button sevenButton;
+    @FXML
+    private Button eightButton;
+    @FXML
+    private Button nineButton;
+    @FXML
+    private Button fourButton;
+    @FXML
+    private Button fiveButton;
+    @FXML
+    private Button sixButton;
+    @FXML
+    private Button oneButton;
+    @FXML
+    private Button twoButton;
+    @FXML
+    private Button threeButton;
+    @FXML
+    private Button zeroButton;
+    @FXML
+    private Button jButton;
+    @FXML
+    private Button cancButton;
+    @FXML
+    private Button spaceButton;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -80,14 +110,89 @@ public class FXMLDocumentController implements Initializable {
         sendIco.setFitWidth(35);
         sendButton.styleProperty().bind(Bindings.when(sendButton.armedProperty()).then("-fx-background-color: #ef5350;").otherwise("-fx-background-color: #ff867c;"));
         
-        for(int i = 0; i<100;i++){
-            try {
-                parser.parse("1.11-1.22j");
-            } catch (InterpreterException ex) {
-                Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
+       
         
+    }
+
+    @FXML
+    private void sevenAction(ActionEvent event) {
+        displayField.setText(displayField.getText()+"7");
+    }
+
+    @FXML
+    private void eightAction(ActionEvent event) {
+        displayField.setText(displayField.getText()+"8");
+    }
+
+    @FXML
+    private void nineAction(ActionEvent event) {
+        displayField.setText(displayField.getText()+"9");
+    }
+
+    @FXML
+    private void fourAction(ActionEvent event) {
+        displayField.setText(displayField.getText()+"4");
+    }
+
+    @FXML
+    private void fiveAction(ActionEvent event) {
+        displayField.setText(displayField.getText()+"5");
+    }
+
+    @FXML
+    private void sixAction(ActionEvent event) {
+        displayField.setText(displayField.getText()+"6");
+    }
+
+    @FXML
+    private void oneAction(ActionEvent event) {
+        displayField.setText(displayField.getText()+"1");
+    }
+
+    @FXML
+    private void twoAction(ActionEvent event) {
+        displayField.setText(displayField.getText()+"2");
+    }
+
+    @FXML
+    private void threeAction(ActionEvent event) {
+        displayField.setText(displayField.getText()+"3");
+    }
+
+    @FXML
+    private void pointAction(ActionEvent event) {
+        displayField.setText(displayField.getText()+".");
+    }
+
+    @FXML
+    private void zeroAction(ActionEvent event) {
+        displayField.setText(displayField.getText()+"0");
+    }
+
+    @FXML
+    private void jAction(ActionEvent event) {
+        displayField.setText(displayField.getText()+"j");
+    }
+
+    @FXML
+    private void cancAction(ActionEvent event) {
+        
+        String text=displayField.getText();
+        String newText=text.substring(0,text.length()-1);
+        displayField.setText(newText);
+        
+        
+    }
+
+    @FXML
+    private void sendAction(ActionEvent event) throws InterpreterException {
+        parser.parse(displayField.getText());
+        System.out.println(stack.getFirst());
+    }
+
+    @FXML
+    private void spaceAction(ActionEvent event) {
+        displayField.setText(displayField.getText()+" ");
     }
 
 }

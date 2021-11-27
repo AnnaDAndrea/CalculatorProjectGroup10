1c9ce6249b110430f6a8180ccc5b8448351d99d5
Claudio De Pisapia <claud@LAPTOP-JJAMBPIQ>
2021-11-27 16:34:08 +0100

    ComplexNumOperatiosTest updated

--- ScientificCalculator/test/test/scentificcalculator/ComplexNumOperationTest.java
+++ ScientificCalculator/test/test/scentificcalculator/ComplexNumOperationTest.java
@@ -7,6 +7,7 @@ package test.scentificcalculator;
 
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.NoSuchElementException;
 import org.apache.commons.math3.complex.Complex;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -60,52 +61,114 @@ public class ComplexNumOperationTest {
     
     @Test
     public void testAdd(){
-        Complex op1 = new Complex(2);
+        Complex op1 = new Complex(6);
         complexNumOperation.insertion(op1);
-        Complex op2 = new Complex(3);
+        Complex op2 = new Complex(7);
         complexNumOperation.insertion(op2);
         complexNumOperation.sum();
-        assertEquals(stack.getFirst(),new Complex(5));
-        //fare la stack.size per vedere se si è ridotto a 1 il contenuto dello stack
-        //fare una somma a 3
-        //vedere le eccezioni se non c'è nessun elemento o se ne ho uno solo
+        assertEquals(stack.getFirst(), new Complex(13));
         
-        Complex op3 = new Complex(-2);
+        Complex op3 = new Complex(2);
         complexNumOperation.insertion(op3);
-        Complex op4 = new Complex(-3);
+        Complex op4 = new Complex(3);
         complexNumOperation.insertion(op4);
+        Complex op5 = new Complex(4);
+        complexNumOperation.insertion(op5);
         complexNumOperation.sum();
-        assertEquals(stack.getFirst(), new Complex(-5));
+        assertEquals(stack.getFirst(),new Complex(7));
+        assertEquals(stack.size(),3);
+        
+    }
+    @Test(expected = NoSuchElementException.class)
+    public void testAddExc(){
+        Complex op1 = new Complex(2);
+        complexNumOperation.insertion(op1);
+        complexNumOperation.sum();
+    
     }
     
     @Test
     public void testSubtraction(){
-        Complex op1 = new Complex(3);
+        Complex op1 = new Complex(7);
         complexNumOperation.insertion(op1);
-        Complex op2 = new Complex(2);
+        Complex op2 = new Complex(6);
         complexNumOperation.insertion(op2);
         complexNumOperation.subtraction();
         assertEquals(stack.getFirst(), new Complex(1));
+        
+        Complex op3 = new Complex(5);
+        complexNumOperation.insertion(op3);
+        Complex op4 = new Complex(4);
+        complexNumOperation.insertion(op4);
+        Complex op5 = new Complex(2);
+        complexNumOperation.insertion(op5);
+        complexNumOperation.subtraction();
+        assertEquals(stack.getFirst(), new Complex(2));
+        assertEquals(stack.size(),3);
+    }
+    
+    @Test(expected = NoSuchElementException.class)
+    public void testSubtractionExc(){
+        Complex op1 = new Complex(2);
+        complexNumOperation.insertion(op1);
+        complexNumOperation.sum();
+    
     }
     
     @Test
     public void testProduct(){
-        Complex op1 = new Complex(4);
+        Complex op1 = new Complex(2);
         complexNumOperation.insertion(op1);
-        Complex op2 = new Complex(5);
+        Complex op2 = new Complex(3);
         complexNumOperation.insertion(op2);
         complexNumOperation.product();
-        assertEquals(stack.getFirst(), new Complex(20));
+        assertEquals(stack.getFirst(), new Complex(6));
+        
+        Complex op3 = new Complex(4);
+        complexNumOperation.insertion(op3);
+        Complex op4 = new Complex(5);
+        complexNumOperation.insertion(op4);
+        Complex op5 = new Complex(3);
+        complexNumOperation.insertion(op5);
+        complexNumOperation.product();
+        assertEquals(stack.getFirst(), new Complex(15));
+        assertEquals(stack.size(), 3);
+    }
+    
+     @Test(expected = NoSuchElementException.class)
+    public void testProductExc(){
+        Complex op1 = new Complex(2);
+        complexNumOperation.insertion(op1);
+        complexNumOperation.product();
+    
     }
     
     @Test
     public void testDivision(){
-        Complex op1 = new Complex(20);
+        Complex op1 = new Complex(6);
         complexNumOperation.insertion(op1);
-        Complex op2 = new Complex(4);
+        Complex op2 = new Complex(2);
         complexNumOperation.insertion(op2);
         complexNumOperation.division();
+        assertEquals(stack.getFirst(), new Complex(3));
+        
+        Complex op3 = new Complex(24);
+        complexNumOperation.insertion(op3);
+        Complex op4 = new Complex(20);
+        complexNumOperation.insertion(op4);
+        Complex op5 = new Complex(4);
+        complexNumOperation.insertion(op5);
+        complexNumOperation.division();
         assertEquals(stack.getFirst(), new Complex(5));
+        assertEquals(stack.size(), 3);
+    }
+    
+    @Test(expected = NoSuchElementException.class)
+    public void testDivisionExc(){
+        Complex op1 = new Complex(2);
+        complexNumOperation.insertion(op1);
+        complexNumOperation.division();
+    
     }
     
     @Test
@@ -116,12 +179,24 @@ public class ComplexNumOperationTest {
         assertEquals(stack.getFirst(), new Complex(5));
     }
     
+    @Test(expected = NoSuchElementException.class)
+    public void testSquareRootExc(){
+        complexNumOperation.squareRoot();
+    
+    }
+    
     @Test
     public void testInvertedSign(){
         Complex op1 = new Complex(3,2);
         complexNumOperation.insertion(op1);
         complexNumOperation.invertedSign();
         assertEquals(stack.getFirst(), new Complex(-3,-2));
+    }
+    
+    @Test(expected = NoSuchElementException.class)
+    public void testInvertedSignExc(){
+        complexNumOperation.invertedSign();
+    
     }
     
 }

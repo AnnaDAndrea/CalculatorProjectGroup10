559a9d1d207af2adfa9b7677fe1cb85e2d8516ad
Carmine Coppola <>
2021-12-01 11:00:00 +0100

    Variables: added [v1.0] VariablesTest: added [v1.0]

---
+++ ScientificCalculator/src/scientificcalculator/Variables.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scientificcalculator;
+
+import java.util.Deque;
+import org.apache.commons.math3.complex.Complex;
+
+/**
+ *
+ * @author Group 10
+ */
+public class Variables {
+    
+    private Deque<Complex> stack;
+
+    
+    
+    
+    
+    
+    
+}

c3a8204a07e6b701a974d6af787c94107324e9da
Anna D'Andrea <>
2021-12-03 19:50:25 +0100

    User Defined Operation: comments added

--- ScientificCalculator/src/scientificcalculator/UserDefinedOperation.java
+++ ScientificCalculator/src/scientificcalculator/UserDefinedOperation.java
@@ -6,9 +6,12 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- *
+ *@brief this class implements the logic needed to maintain a data structure that stores user-defined operations and methods to manage them
+ * 
+ The Map data structure is a key-value map, where the key is the name of the user-defined and the value is the sequence representing the operation
  * @author Group 10
  */
+
 public class UserDefinedOperation{
     private Map<String, String> userOperations;
 
@@ -16,16 +19,27 @@ public class UserDefinedOperation{
         userOperations = new HashMap<>();
     }
 
-
+/**
+ * @brief getNameOperations method returns a set containing all the keys
+ * @return Set<String>
+ */
     public Set<String> getNameOperations() {
         return userOperations.keySet();
     }
 
-
+/**
+ * @brief getSequence method returns the value related to the key
+ * @param op is key
+ * @return 
+ */
     public String getSequence(String op) {
         return userOperations.get(op);
     }
-    
+/**
+ * @brief newOperation method adds a new couple key-value and if the key already exists throws a KeyAlreadyExistException
+ * @param name
+ * @param value 
+ */
     public void newOperation(String name, String value){
         if(userOperations.containsKey(name))
             throw new KeyAlreadyExistException();

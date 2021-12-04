bea521d6199ed285d242b95e20e623eb11209655
Carmine Coppola <>
2021-12-04 17:55:30 +0100

    VarOutOfRangeException: updated

--- ScientificCalculator/src/exception/VarOutOfRangeException.java
+++ ScientificCalculator/src/exception/VarOutOfRangeException.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exception;
 
 /**
  *
  * @author Group 10
+ * @brief This is the Exception called when the variable is not contained in [a, b, ..., z]
  */
 public class VarOutOfRangeException extends RuntimeException {
 

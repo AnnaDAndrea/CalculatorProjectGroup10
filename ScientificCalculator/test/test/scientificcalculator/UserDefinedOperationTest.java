af1ab856b5bf6877aa070ecbe8abebb2fac8f466
Anna D'Andrea <>
2021-12-07 20:12:45 +0100

    UserDefinedOperationTest: delete methods tested

--- ScientificCalculator/test/test/scientificcalculator/UserDefinedOperationTest.java
+++ ScientificCalculator/test/test/scientificcalculator/UserDefinedOperationTest.java
@@ -47,22 +47,67 @@ public class UserDefinedOperationTest
     public void testNewOperation1() {
         userDefinedOperation.newOperation("op2", "3+2j 4+4j swap");
         userDefinedOperation.newOperation("op3", "2j 3j over");
-        
-       
-
+ 
         assertEquals("3+2j 4+4j swap", userDefinedOperation.getSequence("op2"));
-        
-    
-        
+
     }
     
-    
+    /**
+     * @brief testDelete method tests the deletion of an operation
+     */
     @Test
     public void testDelete(){
         userDefinedOperation.newOperation("op1","1+2j 1-2j +");
         userDefinedOperation.newOperation("op2","1-2j 1-2j -");
         
-        assertEquals(userDefinedOperation.delete("op1"),"1+2j 1-2j +");
+        assertEquals("1+2j 1-2j +",userDefinedOperation.delete("op1"));
+        assertNull(userDefinedOperation.delete("op1"));
+    }
+    
+    /**
+     * @brief 
+     */
+    @Test
+    public void testDeleteAllDependencies(){
+       userDefinedOperation.newOperation("op1","1 2 swap"); 
+       userDefinedOperation.newOperation("op2","1 2 op1");
+       userDefinedOperation.newOperation("op3","3 6 op1");
+       userDefinedOperation.newOperation("op4","6 7 op2");
+       userDefinedOperation.newOperation("triplSum","+ + +");
+       userDefinedOperation.newOperation("quadSum","+ triplSum");
+       userDefinedOperation.newOperation("quintSum","triplSum + +");
+       userDefinedOperation.newOperation("op6","1+2j 1+2j +");
+       userDefinedOperation.newOperation("op7","1+2j 1+2j *");
+       
+       
+       userDefinedOperation.deleteAllDependencies("op1");
+       assertNull(userDefinedOperation.getSequence("op1"));
+       assertNull(userDefinedOperation.getSequence("op2"));
+       assertNull(userDefinedOperation.getSequence("op3"));
+       assertNull(userDefinedOperation.getSequence("op4"));
+       assertEquals("1+2j 1+2j +",userDefinedOperation.getSequence("op6"));
+       assertEquals("1+2j 1+2j *",userDefinedOperation.getSequence("op7"));
+       assertEquals("+ + +",userDefinedOperation.getSequence("triplSum"));
+       assertEquals("+ triplSum",userDefinedOperation.getSequence("quadSum"));
+       assertEquals("triplSum + +",userDefinedOperation.getSequence("quintSum"));
+        
+       userDefinedOperation.deleteAllDependencies("quadSum");
+       assertEquals("1+2j 1+2j +",userDefinedOperation.getSequence("op6"));
+       assertEquals("1+2j 1+2j *",userDefinedOperation.getSequence("op7"));
+       assertEquals("+ + +",userDefinedOperation.getSequence("triplSum"));
+       assertEquals("triplSum + +",userDefinedOperation.getSequence("quintSum"));
+       
+       userDefinedOperation.deleteAllDependencies("triplSum");
+       assertEquals("1+2j 1+2j +",userDefinedOperation.getSequence("op6"));
+       assertEquals("1+2j 1+2j *",userDefinedOperation.getSequence("op7"));
+       assertNull(userDefinedOperation.getSequence("triplSum"));
+       assertNull(userDefinedOperation.getSequence("quintSum"));
+       
+       userDefinedOperation.deleteAllDependencies("op6");
+       assertNull(userDefinedOperation.getSequence("op6"));
+       assertEquals("1+2j 1+2j *",userDefinedOperation.getSequence("op7"));
+       
+ 
     }
   
 }

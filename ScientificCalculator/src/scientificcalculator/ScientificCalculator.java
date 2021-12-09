252fc6328e3076137066839cf614deb289c4425f
Antonello Avella <>
2021-12-09 19:43:02 +0100

    Icon added

--- ScientificCalculator/src/scientificcalculator/ScientificCalculator.java
+++ ScientificCalculator/src/scientificcalculator/ScientificCalculator.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -14,7 +15,7 @@ public class ScientificCalculator extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+        stage.getIcons().add(new Image("/img/calcIcon.png"));
         Scene scene = new Scene(root);
         stage.setResizable(false);
         stage.setTitle("Scientific Calculator");
